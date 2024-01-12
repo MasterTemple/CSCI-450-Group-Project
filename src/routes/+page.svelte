bf528ff3d311d2d5150f603813317d94ac77f1a9
Blake Scampone <blake.scampone@gmail.com>
2024-01-11 21:56:06 -0800

    Created major sections

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<nav id="navbar">
+</nav>
+<div id="content">
+    <div id="saved"></div>
+    <div id="editor"></div>
+    <div id="settings"></div>
+</div>
+
+<style>
+
+/* :root {
+    display: flex;
+    flex-direction: row;
+} */
+
+#navbar {
+    /* position: fixed; */
+    width: 100vw;
+    height: 12vh;
+    background-color: red;
+}
+
+#content {
+    display: flex;
+    flex-direction: row;
+    height: 88vh;
+    width: 100vw;
+}
+
+#saved {
+    height: 100%;
+    width: 20vw;
+    background-color: green;
+}
+
+#editor {
+    background-color: grey;
+    height: 100%;
+    width: 60vw;
+}
+
+#settings {
+    background-color: blue;
+    height: 100%;
+    width: 20vw;
+}
+
+</style>
