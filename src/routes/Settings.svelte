db10b335e4a686a068a19fb7b44077f1a20e6197
Ellieanna Ross <74512916+ellieannar@users.noreply.github.com>
2024-02-13 18:40:00 -0800

    tab to control settings menu

--- src/routes/Settings.svelte
+++ src/routes/Settings.svelte
@@ -7,11 +7,18 @@
         console.log("Toggle Drawer:" + $settingsIsOpen)
         settingsIsOpen.set(!$settingsIsOpen)        
     }
+
+
 </script>
 
 
 <div id="drawerDiv">
     <button id="drawerButton" on:click={toggleDrawer} >{icons[$settingsIsOpen ? 1 : 0]}</button>
+</div>
+
+<div id="editorOrPresentationToggle">
+    <button id="editorToggle">Editor</button>
+    <button id="presentationToggle">Presentation</button>
 </div>
 
 
@@ -32,5 +39,29 @@
   border-top-left-radius: 10px; 
   border-bottom-left-radius: 10px;
 }
+
+#editorOrPresentationToggle {
+    display: flex;
+}
+
+#editorToggle {
+    flex: 1;
+    width: 50%;
+    position: relative;
+    background-color: red;
+    border: none;
+    height: 50px;
+    border-top-left-radius: 19px;
+}
+
+#presentationToggle {
+    flex: 1;
+    position: relative;
+    width: 50%;
+    background-color: rgb(129, 0, 0);
+    border: none;
+    height: 50px;
+}
+
 
 </style>
