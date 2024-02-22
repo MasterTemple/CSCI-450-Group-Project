f8ff5d43cd6a9ddb6aa7cf2bc7cec1d98d91eb2a
Blake Scampone <blake.scampone@gmail.com>
2024-02-22 14:54:52 -0800

    Fixed color import from store.js

--- src/routes/LyricEditor.svelte
+++ src/routes/LyricEditor.svelte
@@ -2,7 +2,7 @@
     import { writable } from 'svelte/store';
     import { EXAMPLE_CONTENTS_2 } from "./constants.js"
     import { splitLine } from "./functions.js"
-    import { rawClipboardContents, lines, numberOfColumnsToDisplay, dividerList, breakIndexes } from "./stores.js"
+    import { rawClipboardContents, lines, numberOfColumnsToDisplay, dividerList, breakIndexes, color } from "./stores.js"
     import { onMount } from 'svelte';
 
     onMount(() => {
