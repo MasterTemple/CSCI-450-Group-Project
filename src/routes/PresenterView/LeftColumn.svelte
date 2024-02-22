2a96848387ed3618d97b9d6a9fd24e6382527344
Joaquin Llenado <joaquinllenado@gmail.com>
2024-02-22 15:03:52 -0800

    Added SlidePreview on RightColumn

--- src/routes/PresenterView/LeftColumn.svelte
+++ src/routes/PresenterView/LeftColumn.svelte
@@ -1,21 +1,24 @@
 <script>
-    //window.addEventListener("keyup", function(e){ if(e.keyCode == 27) history.back();}, false);
+    import { color } from '../stores.js';
+
+    //Send user back to editor page when ESC key is pressed 
+    //window.addEventListener("keyup", function(e){ if(e.keyCode == 27) this.window.location.assign("../");}, false);
 </script>
 
 
 <a href=".././">
-    <button id="backButton">Back</button>
+    <button id="backButton" style="--color: {color.darkBlue}">Back</button>
 </a>
 
 
-<button id="switchSong">Switch Song</button>
+<button id="switchSong" style="--color: {color.darkBlue}">Switch Song</button>
 
 <div id = "songSlideStack">
     <div id="songInfo">
-        <div id="songTitle">
+        <div id="songTitle" style="--color: {color.darkBlue}">
             <p>Song Title</p>
         </div>
-        <div id="songArtist">
+        <div id="songArtist" style="--color: {color.darkBlue}">
             <p>Song Artist</p>
         </div>
     </div>
@@ -36,7 +39,9 @@
         border-radius: 10px;
         height: 50px;
         width: 150px;
-        background-color: darkgray;
+        background-color: var(--color);
+        color: white;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     }
 
 
@@ -48,7 +53,9 @@
         border-radius: 10px;
         height: 50px;
         width: 150px;
-        background-color: darkgray;
+        background-color: var(--color);
+        color: white;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     }
 
 
@@ -68,31 +75,33 @@
 
 
     #songTitle {
-        border: 2px solid black;
-        background-color: lightgray;
+        border: none;
+        background-color: var(--color);
+        color: white;
         border-radius: 5px;
         height: 40px;
         width: 150px;
         text-wrap:balance;
-        margin: 5px;
+        font-size: larger;
     }
 
 
     #songArtist {
-        border: 2px solid black;
-        background-color: lightgray;
+        border: none;
+        background-color: var(--color);
+        color: white;
         border-radius: 5px;
         height: 40px;
-        width: 100px;
+        width: 130px;
         text-wrap: balance;
+        font-size: larger;
         margin: auto;
     }
 
 
     #currentSlide {
-        border: 4px solid black;
+        border: 4px solid darkgreen;
         border-radius: 3%;
-        border-color: green;
         height: 30vw;
         width: 50vw;
         padding: 10px;
