452b68e7c5b58cb16bd17a2449fd92e5b58d5779
Blueflier <joseph.e.hartono@gmail.com>
2024-03-07 14:11:33 -0800

    added css to editor settings

--- src/routes/EditorSettings.svelte
+++ src/routes/EditorSettings.svelte
@@ -19,20 +19,22 @@
 </script>
 
 <div id="editor-settings-style">
-    <div>
+    <div class="editor-settings-items">
+        <label>Font Size<input  name="editor-settings" type="number" min="12" max="36" id="fontSize" bind:value={$fontSize}></label>
+    </div>
+    <div class="editor-settings-items" >
+        <!-- <label>hello</label> -->
         <label>Text<input name="editor-settings" type="color" id="textColor" bind:value={$textColor}></label>
     </div>
     
-    <div>
+    <div  class="editor-settings-items">
         <label>Background<input name="editor-settings" type="color" id="backgroundColor" bind:value={$backgroundColor}></label>
     </div>
     
-    <div>
-        <label>Font Size<input name="editor-settings" type="number" min="12" max="36" id="fontSize" bind:value={$fontSize}></label>
-    </div>
+
     
-    <div>
-        <label for="Font Family">Font Family</label>
+    <div class="editor-settings-items" >
+        <label for="Font Family">Font</label>
         <select name = "editor-settings" id="fontFamily" bind:value={$fontFamily}>
             <option value=arial selected>Arial</option>
             <option value=timeNewRoman>Times New Roman</option>
@@ -40,11 +42,12 @@
         </select>
     </div>
     
-    <div>
+    <div class="editor-settings-items" >
         <label>Title Slide<input name="editor-settings" type="checkbox" id="titleSlide" bind:value={$titleSlide}></label>
     </div>
+
     
-    <div>
+    <div class="editor-settings-items" >
         <label>Columns <input name="editor-settings" type="number" min="1" max="6" id="numberOfColumns" bind:value={$numberOfColumnsToDisplay}></label>
     </div>
     
@@ -68,15 +71,30 @@
     
 }
 #editor-settings-style{
-    padding-left: 15px;
-    padding-top: 15px;
+
     display: flex;
     flex-direction: column;
 }
 #textColor{
-    /* margin-left: 15px; */
+    margin-right: 5px;
     float: right;
-    /* padding-right:15px; */
+}
+#backgroundColor{
+    margin-right: 5px;
+    float: right;
+}
+
+#fontSize{
+    margin-right: 15px;
+    float: right;
+
+}
+
+.editor-settings-items{
+    padding-bottom: 15px;
+    padding-top: 15px;
+    margin-left: 30px;
+    font-size: 20px;
 }
 
 
