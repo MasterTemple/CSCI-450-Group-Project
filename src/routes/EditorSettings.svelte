9672c675f4dcefbf99476c860588341aaa8225df
Ellieanna Ross <74512916+ellieannar@users.noreply.github.com>
2024-04-04 16:17:47 -0700

    editing settings

--- src/routes/EditorSettings.svelte
+++ src/routes/EditorSettings.svelte
@@ -260,4 +260,31 @@
 		-ms-user-select: none;
 		user-select: none;
 	}
+
+	input[type="radio"] {
+	appearance: none;
+	-webkit-appearance: none;
+	width: 20px;
+	height: 20px;
+	border: 1px solid #fff;
+	border-radius: 50%;
+	margin-right: 10px;
+	background-color: transparent;
+	position: relative;
+	top: 6px;
+}
+
+input[type="radio"]:checked::before {
+	content: "";
+	display: block;
+	width: 12px;
+	height: 12px;
+	background-color: #ca8489;
+	border-radius: 50%;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	animation: appear 0.8s;
+}
 </style>
