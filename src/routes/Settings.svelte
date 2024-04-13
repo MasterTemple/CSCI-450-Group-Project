97923f115aede1309acb911cb0c27a6e5f4950f4
Blake Scampone <54373793+MasterTemple@users.noreply.github.com>
2024-04-13 10:49:45 -0700

    settings buttons

--- src/routes/Settings.svelte
+++ src/routes/Settings.svelte
@@ -2,19 +2,8 @@
 	import { writable } from "svelte/store";
 	import EditorSettings from "./EditorSettings.svelte";
 	import PresentationSettings from "./PresentationSettings.svelte";
-	import {
-		color,
-		settingsIsOpen,
-		settingsSelection,
-		settingsToggleOption,
-	} from "./stores";
-
-	let icons = ["<", ">"];
 
 	let isEditorSettings = writable(true);
-	function settingsToggle() {
-		isEditorSettings.set(!$isEditorSettings);
-	}
 </script>
 
 <div id="settings-list">
@@ -53,21 +42,18 @@
 	}
 
 	#settings-section-select > button {
-		background-color: var(--dark2);
 		flex: 1;
 		width: 50%;
-		position: relative;
 		color: var(--white);
-		border: none;
 		height: 3rem;
 		font-size: medium;
-		border-top-left-radius: calc(2 * var(--border-radius));
-		border-top-right-radius: calc(2 * var(--border-radius));
+		background-color: var(--dark1);
+		border: 2px solid var(--dark5);
+		border-radius: calc(2 * var(--border-radius));
+		margin: 4px;
 	}
 	#settings-section-select > button.selected {
-		background-color: black;
-		border-top-left-radius: 10px;
-		border-bottom-left-radius: 10px;
+		border-color: var(--secondary);
 	}
 
 	#settings-content {
